fc45e4c4872d7501d9948bf107b4bb1cb6a5ec93
Dhaval Thakkar <dhavalthakkar4995@gmail.com>
2019-02-27 01:12:15 +0530

    Header Footer Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './src/app/Common/header/header.component';
-import { FooterComponent } from './src/app/Common/footer/footer.component';
+import { HeaderComponent } from './Common/header/header.component';
+import { FooterComponent } from './Common/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,    
+    FooterComponent,      
   ],
   imports: [
     BrowserModule,
